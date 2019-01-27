a6d9d306bca032f0ea9f9a7db4c9d7b7971901e7
Jim Larsson <jim.larsson.dev@gmail.com>
2019-01-27 19:38:02 +0100

    experimental

--- app/src/main/java/com/jimbodev/jimla/projectg/Buildable.java
+++ app/src/main/java/com/jimbodev/jimla/projectg/Buildable.java
@@ -9,19 +9,22 @@ import android.graphics.Rect;
 class Buildable extends Vector {
     private Bitmap bitmap;
     private Rect source, dest;
-    private int sourceX = 1, sourceY = 3;
-    private int w = 168;
+    private int sourceX = 1, sourceY = 1;
+    private int w = 64, h = 64;
     Buildable(float x, float y, Resources resources) {
         super(x, y);
 
         bitmap = BitmapFactory.decodeResource(
                 resources,
-                R.drawable.table2
+                R.drawable.objects_table
         );
+        //bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight());
+        //bitmap = Bitmap.createScaledBitmap(bitmap, w * 8, h, false);
 
-        source = new Rect(sourceX, sourceY, sourceX + w, sourceY + w);
-        dest = new Rect((int) x, (int) y, (int) x + w, (int) y + w); //TODO * ratio
-        //dest = new Rect((int) x, (int) y, (int) x + w * 2, (int) y + w * 2); //TODO * ratio
+        //source = new Rect(sourceX, sourceY, sourceX + w, sourceY + w);
+        //dest = new Rect((int) x, (int) y, (int) x + w, (int) y + w); //TODO * ratio
+        source = new Rect(0, 0, w, h);
+        dest = new Rect(0, 0, w, h); //TODO * ratio
     }
 
     void show(Canvas canvas) {
